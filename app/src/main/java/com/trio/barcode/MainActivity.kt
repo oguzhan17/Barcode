12747bf0c29ead4ebebe8714c8dc7cd90ac29d6a
oguzhan17 <oguzhancevher17@gmail.com>
2020-02-05 12:24:15 +0300

    Barcode finished

--- app/src/main/java/com/trio/barcode/MainActivity.kt
+++ app/src/main/java/com/trio/barcode/MainActivity.kt
@@ -1,12 +1,110 @@
 package com.trio.barcode
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.WorkSource
+import android.util.SparseArray
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.util.isNotEmpty
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestCodeCameraPermission = 1001
+    private lateinit var cameraSource: CameraSource
+    private lateinit var detector: BarcodeDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                android.Manifest.permission.CAMERA
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            askForCameraPermmission()
+        } else {
+            setupControls()
+        }
+
+    }
+
+    private fun setupControls() {
+        detector = BarcodeDetector.Builder(this@MainActivity).build()
+        cameraSource = CameraSource.Builder(this@MainActivity, detector)
+            .setAutoFocusEnabled(true)
+            .build()
+        cameraSurfaceView.holder.addCallback(surfaceCallBack)
+        detector.setProcessor(processor)
+    }
+
+    private fun askForCameraPermmission() {
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(android.Manifest.permission.CAMERA),
+            requestCodeCameraPermission
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestCodeCameraPermission && grantResults.isNotEmpty()) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setupControls()
+            } else {
+                Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private val surfaceCallBack = object : SurfaceHolder.Callback {
+        override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun surfaceDestroyed(p0: SurfaceHolder?) {
+            cameraSource.stop()
+        }
+
+        override fun surfaceCreated(surfaceHolder: SurfaceHolder?) {
+            try {
+                cameraSource.start(surfaceHolder)
+            } catch (exception: Exception) {
+                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+    private val processor = object : Detector.Processor<Barcode>{
+        override fun release() {
+
+        }
+
+        override fun receiveDetections(detections: Detector.Detections<Barcode>?) {
+
+            if(detections !=null && detections.detectedItems.isNotEmpty()){
+                val qrCodes: SparseArray<Barcode> = detections.detectedItems
+                val code = qrCodes.valueAt(0)
+                textScanResult.text = code.displayValue
+            }else{
+                textScanResult.text = ""
+            }
+        }
     }
 }
